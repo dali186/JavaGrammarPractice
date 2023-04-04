41d89993a7b9e4aff8d2a9fadad0fa9e59221358
JOOWON <wer07032@naver.com>
2023-04-04 19:05:18 +0900

    Boj, Stream

---
+++ WorkSpace/src/main/java/AssitanceStream/FileCopyTest.java
@@ -0,0 +1,29 @@
+package AssitanceStream;
+
+import java.io.*;
+
+public class FileCopyTest {
+
+    public static void main(String[] args) {
+
+        long millisecond = 0;
+
+        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream("reader.txt"));
+             BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("copy.txt"))){
+
+            millisecond = System.currentTimeMillis();
+
+            int i;
+            while ((i = bis.read())!=-1) {
+                bos.write(i);
+            }
+
+            millisecond = System.currentTimeMillis() - millisecond;
+
+        }catch(IOException e){
+
+            e.printStackTrace();
+        }
+        System.out.println("end : " + millisecond);
+    }
+}
